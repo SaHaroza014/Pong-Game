b271ce836bd4ebec211eecdd7cde640d2ae57540
Sanjin Hadžalić <80854488+SaHaroza014@users.noreply.github.com>
2022-08-03 12:00:23 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from play_ball import Ball
+from scoreboard import Score
+import time
 
 screen = Screen()
 screen.title("Pong Game")
 screen.setup(height=600, width=1000)
+screen.tracer(0)
 screen.bgcolor('black')
 
-paddle = Paddle()
+right_paddle = Paddle((440, 0))
+left_paddle = Paddle((-440, 0))
+ball = Ball()
+score = Score()
 
 screen.listen()
+screen.onkeypress(right_paddle.move_up, 'Up')
+screen.onkeypress(right_paddle.move_down, 'Down')
+screen.onkeypress(left_paddle.move_up, 'w')
+screen.onkeypress(left_paddle.move_down, 's')
 
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_spd)
+    screen.update()
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 330 or ball.distance(left_paddle) < 50 and ball.xcor() < -330:
+        ball.bounce_x()
 
+    if ball.xcor() > 480:
+        score.l_point()
+        score.update()
+        ball.reset()
 
+    if ball.xcor() < -480:
+        score.r_point()
+        score.update()
+        ball.reset()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,21 +1,21 @@
 from turtle import Turtle
 
-POSITIONS = [(440, 20), (440, 0), (440, -20), (440, -40)]
 
+class Paddle(Turtle):
+    def __init__(self, positions):
+        super().__init__()
+        self.shape('square')
+        self.penup()
+        self.color('white')
+        self.turtlesize(stretch_wid=6, stretch_len=2)
+        self.goto(positions)
 
-class Paddle:
-    def __init__(self):
-        self.segments = []
-        self.create_paddle()
+    def move_up(self):
+        y = self.ycor()
+        y += 20
+        self.sety(y)
 
-    def create_paddle(self):
-        for segment in POSITIONS:
-            paddle = Turtle(shape='square')
-            paddle.hideturtle()
-            paddle.color('white')
-            paddle.penup()
-            paddle.setpos(segment)
-            paddle.showturtle()
-            self.segments.append(segment)
-
-
+    def move_down(self):
+        y = self.ycor()
+        y -= 20
+        self.sety(y)
